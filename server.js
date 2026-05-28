85a2fd1d01755a4bd172182b084e5b49420610e3
simonegreco5 <simoxey.greco@gmail.com>
2026-05-28 20:47:59 +0200

    - add static assets and define root route in server.js with a simple text

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+// register the static assets 
+app.use(express.static('public'))
+
+// start the server
+app.listen(port, () => {
+    console.log(`Server lidtening http://localhost:${port}`)
+})
+
+// define routes here
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
